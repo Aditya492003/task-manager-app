187e1c6f81cdd69e8461a68f4cb5b7e40211ab50
Aditya492003 <124594698+Aditya492003@users.noreply.github.com>
2026-06-04 15:06:28 +0530

    backend started

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+
+const app = express() 
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/' , (req , res ) => res.send('server is live!!'))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , () => {
+    console.log('server is running on' , PORT );
+})
